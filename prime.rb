c74972d57bc98a6658c3a12cfa2a52a570756b4f
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-02-20 22:39:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 def prime?(number)
-  (2..((number * -1) - 1)).each do |num|
-    return false if number % num == 0 
+  (2..(number - 1)).each do |num|
+    return false if (number * -1) % num == 0 
     end
     true
   end
